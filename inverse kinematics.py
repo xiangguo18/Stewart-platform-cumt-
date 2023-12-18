941499ff7b47916bf82f569e33764c35b39a892c
xiangguo18 <135950305+xiangguo18@users.noreply.github.com>
2023-12-18 21:55:23 +0800

    Add files via upload

--- inverse kinematics.py
+++ inverse kinematics.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-def rotx(phi): 
-    if phi > np.pi:
-        phi = np.radians(phi)
+def rotx(phi):                #输入度数
+    phi = np.radians(phi)
     rotX = np.array([
     [1, 0, 0],
     [0, np.cos(phi), -np.sin(phi)],
@@ -12,8 +11,7 @@ def rotx(phi):
     return rotX 
  
 def roty(theta): 
-    if theta > np.pi:
-        theta  = np.radians(theta)
+    theta  = np.radians(theta)
     rotY = np.array([
     [np.cos(theta), 0, np.sin(theta)],
     [0, 1, 0],
@@ -22,8 +20,7 @@ def roty(theta):
     return rotY  
 
 def rotz(psi): 
-    if psi  > np.pi:
-        psi = np.radians(psi)
+    psi = np.radians(psi)
     rotZ = np.array([
     [np.cos(psi), -np.sin(psi), 0],
     [np.sin(psi), np.cos(psi), 0],
@@ -31,147 +28,155 @@ def rotz(psi):
     ])
     return rotZ 
 
+def atan(a):               #返回弧度
+    return np.arctan(a)
+
+def atand(a):              #返回角度
+    pi=np.pi
+    return np.arctan(a)*180/pi
+
+def cos(radian):
+    return np.cos(radian)
+
+def sin(radain):
+    return np.sin(radain)
+
 
 """
 计算平台参数
 """
-
 #输入参数
-r_b = 70
-r_p = 54
+servo_arm = np.array([23,23,23,23,23,23])
+leg =  np.array([126,126,126,126,126,126])                   #rod or leg   126 is leg 
 
-servo_arm = 23
-leg =  126                    #rod or leg   126 is leg 
-
-alpha_b = 27.4307
-
-alpha_p = 16.6714
-
-home = np.array([                   #零位  位移  旋转
-    0,
-    0,
-    127
-    ])
-orint = np.array([
-    0,
-    0,
-    0
-    ])
-trans = np.array([
-    0,
-    0,
-    0
-    ])                  
-
+                                  #零位  位移  旋转
+ 
+home=np.array([[0],[0],[123.12]])       #舵机-球铰123.12，舵机- 顶面127.12，舵机-底面23.15，顶面-底面15.27，顶面-球铰4      
+orint=np.array([[0],[0],[0]])
+trans=np.array([[0],[0],[0]])
 
 #定义平台几何尺寸
-pi = np.pi
-beta = np.array([
-    pi + pi / 2, 
-    pi / 2,
-    2 * pi / 3 + pi + pi / 2, 
-    2 * pi / 3 + pi / 2,
-    4 * pi / 3 + pi + pi / 2, 
-    4 * pi / 3 + pi / 2
+servo_b = np.array([
+    [-30, -59.3, 0],
+    [30, -59.3 , 0],
+    [66.36,  3.67, 0],
+    [36.36, 55.63, 0],
+    [-36.36, 55.63, 0],
+    [-66.36, 3.67, 0]
+])
+rod_p = np.array([
+    [-39.34, -40.03, 0],
+    [39.34, -40.03 , 0],
+    [54.34,  -14.05, 0],
+    [15, 54.08, 0],
+    [-15, 54.08, 0],
+    [-54.34, -14.05, 0]
 ])
 
-theta_b = np.array([
-    alpha_b,
-    alpha_b,
-    pi/3+alpha_b,
-    pi/3-alpha_b,
-    pi/3-alpha_b,
-    pi/3+alpha_b
-    ])
-
-theta_p = np.array([
-    pi/3-alpha_p,
-    pi/3-alpha_b,
-    pi/3+alpha_b,
-    alpha_b,
-    alpha_b,
-    pi/3+alpha_b
-    ])
-
-servo_b = r_b * np.array([
-    [np.cos(theta_b[0]), -np.sin(theta_b[0]), 0],
-    [np.cos(theta_b[1]),  np.sin(theta_b[1]), 0],
-    [-np.cos(theta_b[2]), np.sin(theta_b[2]), 0],
-    [-np.cos(theta_b[3]), np.sin(theta_b[3]), 0],
-    [-np.cos(theta_b[4]), -np.sin(theta_b[4]), 0],
-    [-np.cos(theta_b[5]), -np.sin(theta_b[5]), 0]
-])
-rod_p = r_p * np.array([
-    [np.cos(theta_p[0]), -np.sin(theta_p[0]), 0],
-    [np.cos(theta_p[1]),  np.sin(theta_p[1]), 0],
-    [np.cos(theta_p[2]),  np.sin(theta_p[2]), 0],
-    [-np.cos(theta_p[3]), np.sin(theta_p[3]), 0],
-    [-np.cos(theta_p[4]), -np.sin(theta_p[4]), 0],
-    [np.cos(theta_p[5]), -np.sin(theta_p[5]), 0]
-])
-"""
-print('servo_b is /n',servo_b)
-print('rod_p is /n',rod_p)
-print(rod_p[:,1]) 
+#print('servo_b is : ')
+#print(servo_b)
+#print('rod_p is :')
+#print(rod_p)
 
 fig = plt.figure()                                 #创建图对象
 ax = fig.add_subplot(111, projection='3d')         #三维图
-ax.plot(servo_b[:,0], servo_b[:,1], servo_b[:,2], '-')
+ax.plot(servo_b[:,0], servo_b[:,1], servo_b[:,2], '-', marker='o', color="blue")
+ax.plot((servo_b[0,0],servo_b[5,0]), (servo_b[0,1],servo_b[5,1]), (servo_b[0,2],servo_b[5,2]), '-', color="blue")
+
+#ax.plot(rod_p[:,0], rod_p[:,1], rod_p[:,2], '-', marker='o', color="black")
+#ax.plot((rod_p[0,0],rod_p[5,0]), (rod_p[0,1],rod_p[5,1]), (rod_p[0,2],rod_p[5,2]), '-', color="black")
+
 plt.grid(True)
 plt.show()
 
-"""
+
 '''
 计算  有效杆长(电动缸长度) rod
 '''
 
 #计算旋转矩阵
-T_BP = np.dot(rotz(orint[2]),np.dot(roty(orint[1]),rotx(orint[0])))        #T_BP 有错可能   phi theta psi 换成 orient
+T_BP = rotz(orint[2,0]) @ roty(orint[1,0]) @ rotx(orint[0,0])        
+print("T_BP:")
+print (T_BP)
 
-#计算rod_p
-rod_b = np.dot(T_BP,rod_p) + home + trans     
+
+#计算rod_b
+broke_rod_p = np.split(rod_p, rod_p.shape[0])    #拆分成6个1*3
+print("broke_rod_p:")
+print (broke_rod_p)
+brokeT_rod_p = [np.transpose(every_rod_p) for every_rod_p in broke_rod_p]
+print("brokeT_rod_p:")
+print (brokeT_rod_p)
+rodT_b = [T_BP@everyT_rod_p+home+trans for everyT_rod_p in brokeT_rod_p]   #得出rod_b
+print("rodT_b:")
+print (rodT_b)
+rod_b = np.vstack([np.transpose(matrix) for matrix in rodT_b])
+print("rod_b:")
+print (rod_b)
+
 
 #计算rod
-L_B = rod_b - servo_b    
-rod = np.linalg.norm(L_B)                          #输出
+L_B = rod_b - servo_b   
+print("L_B:",type(L_B))
+print (L_B) 
+rod = np.array([np.linalg.norm(a) for a in L_B])                        #输出B
+print("rod:",type(rod))
+print(rod)
 
-#test
-print("T_BP:",T_BP)
-print(np.linalg.norm([1,2,3]))
 
 
 
 '''
 计算  舵机转角 angle
 '''
+def atan(angle):
+    return np.arctan(angle)
+pi = np.pi
 
- #输入
-beta =     #角度值
-beta = np.radions(beta)
+beta = np.array([
+    atan(servo_b[0,1]/servo_b[0,0]), 
+    atan(servo_b[1,1]/servo_b[1,0]),
+    atan(servo_b[2,1]/servo_b[2,0]),
+    atan(servo_b[3,1]/servo_b[3,0]),
+    atan(servo_b[4,1]/servo_b[4,0]),
+    atan(servo_b[5,1]/servo_b[5,0]),
+])
+
+beta_d = np.array([
+    atand(servo_b[0,1]/servo_b[0,0]), 
+    atand(servo_b[1,1]/servo_b[1,0]),
+    atand(servo_b[2,1]/servo_b[2,0]),
+    atand(servo_b[3,1]/servo_b[3,0]),
+    atand(servo_b[4,1]/servo_b[4,0]),
+    atand(servo_b[5,1]/servo_b[5,0]),
+])
+print('betad')
+print(beta)
+print()
 
  #获得坐标
-x_p = rod_p[0:]
-y_p = rod_p[1:]
-z_p = rod_p[2:]
+x_p = rod_p[:,0]
+y_p = rod_p[:,1]
+z_p = rod_p[:,2]
 
-x_b = rod_b[0:]
-y_b = rod_b[1:]
-z_b = rod_b[2:]
+x_b = rod_b[:,0]
+y_b = rod_b[:,1]
+z_b = rod_b[:,2]
 
- #更新舵机转角     矩阵运算还是数值运算？
-rod2 = rod * rod        # matlab/数学运算中.*是等于numpy库中的矩阵A*矩阵B
-leg2 = leg * leg
-arm2 = servo_arm * servo_arm
 
-l = rod2 - leg2 + arm2
+ #更新舵机转角     
+l = rod**2 - leg**2 + servo_arm**2
 M = 2 * servo_arm * (z_p - z_b)
-N = 2 * servo_arm * (np.cos(beta) * (x_p - x_b) + np.sin(beta) * (y_p - y_b))   # .*?
-M2 = M*M
-N2 = N*N
+N = 2 * servo_arm * (cos(beta) * (x_p - x_b) + sin(beta) * (y_p - y_b))   # .*?
+M2 = M**2
+N2 = N**2
+
 
 #计算角度
-angle = np.arcsin(l / (M2 - N2)**0.5) -np.arctan(N,M)     #输出
-
+angle = np.arcsin(l / np.sqrt(M2 - N2)) -np.arctan(N,M)     #输出
+print('angle:',type(angle))
+print(angle)
+'''
 angles = np.zeros(6)
 joint_b = np.zeros((3, 6))
 
@@ -187,6 +192,8 @@ for i in range(6):
         servo_arm * np.cos(angles[i]) * np.sin(beta[i]) + servo_b[1, i],
         servo_arm * np.sin(angles[i])
     ]
+
+
 
 """
 #画图
@@ -227,5 +234,4 @@ for i in range(6):
 # 启用交互式旋转
 plt.grid(True)
 plt.show()
-
-
+'''
