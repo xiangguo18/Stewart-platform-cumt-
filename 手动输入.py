9d42d191f2331d943a9dbf0113c3f950b8f0d9f2
xiangguo18 <135950305+xiangguo18@users.noreply.github.com>
2024-04-26 11:16:03 +0800

    Add files via upload

--- 手动输入.py
+++ 手动输入.py
@@ -39,55 +39,25 @@ def send_modbus_rtu_request(request):
     except serial.SerialException as e:
         print("串口错误:", e)
 
-def main():
-    while True:
-        try:
-            # 手动输入带 CRC 校验码的 Modbus RTU 报文
-            hex_string = input("请输入带 CRC 校验码的十六进制形式的 Modbus RTU 报文，以空格分隔每个字节，输入 q 退出：\n")
-            if hex_string.lower() == 'q':
-                break
 
-            hex_bytes = bytearray.fromhex(hex_string.replace(" ", ""))
-            send_modbus_rtu_request(hex_bytes)
-        except ValueError:
-            print("请输入有效的十六进制字符串。")
+while True:
+    try:
+        # 手动输入带 CRC 校验码的 Modbus RTU 报文
+        hex_string = input("请输入不带 CRC 校验码的十六进制形式的 Modbus RTU 报文，以空格分隔每个字节，输入 q 退出：\n")
+        if hex_string.lower() == 'q':
+            break
+        hex_bytes = bytearray.fromhex(hex_string.replace(" ", ""))
+        crc = calculate_crc(hex_bytes)
+        print("计算得到的 CRC 校验码为:", crc)
+        print(hex_string)
 
-if __name__ == "__main__":
-    main()
+        # 添加 CRC 校验码
+        hex_crc_string = hex_string + " " + crc
+        print("带 CRC 校验码的十六进制输入为:", hex_crc_string)
+
+        hex_crc_bytes = bytearray.fromhex(hex_string.replace(" ", ""))
+        #send_modbus_rtu_request(hex_crc_bytes)
+    except ValueError:
+        print("请输入有效的十六进制字符串。")
 
 
-
-
-
-
-
-'''CRC校验码计算'''
-def calculate_crc(data):
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= byte
-        for _ in range(8):
-            if crc & 0x0001:
-                crc >>= 1
-                crc ^= 0xA001
-            else:
-                crc >>= 1
-    # 返回 CRC 校验码，低八位在前，两位两位以空格隔开
-    return "{:02X} {:02X}".format((crc & 0xFF), ((crc & 0xFF00) >> 8))
-
-def main():
-    while True:
-        try:
-            # 手动输入需要计算 CRC 的数据，以空格分隔每个字节
-            hex_string = input("请输入需要计算 CRC 的十六进制数据，以空格分隔每个字节，输入 q 退出：\n")
-            if hex_string.lower() == 'q':
-                break
-
-            hex_bytes = bytearray.fromhex(hex_string.replace(" ", ""))
-            crc = calculate_crc(hex_bytes)
-            print("计算得到的 CRC 校验码为:", crc)
-        except ValueError:
-            print("请输入有效的十六进制字符串。")
-
-if __name__ == "__main__":
-    main()
